6ffa1616ea290fbab5a00fe54d4b1ab238a858f8
Reinis <reinis.podkalns@parts-advisor.com>
2018-07-02 20:35:39 +0300

    First commit

--- reporter.rb
+++ reporter.rb
@@ -1,12 +1,19 @@
 require_relative 'features/features/support/api_helper'
 require 'json'
 
+job_name = ARGV[0]
+job_number = ARGV[1]
+
 thumbnail = {'url' => 'https://pbs.twimg.com/profile_images/662215297378574336/YOOD0ijr_400x400.png'}
 
 fields = []
 
 fields.push({'name' => 'Autors', 'value' => 'Reinis'})
 fields.push({'name' => 'Mērķis', 'value' => 'REST kurss'})
+
+#pieliek jaunus atgumentus - izsauc ar ruby reporte.rb SampleName SmapleNumber (strings), padod jenkins configure
+fields.push({'name' => 'Job', 'value' => job_name})
+fields.push({'name' => 'Build number', 'value' => job_number})
 
 embed = []
 
